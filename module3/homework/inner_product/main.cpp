3599e1657de030c80f6f9bc6360f460e5a5b0d81
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-09-28 00:49:11 +0200

    try-catch block added in main

--- module3/homework/inner_product/main.cpp
+++ module3/homework/inner_product/main.cpp
@@ -5,9 +5,14 @@
 int main() {
 
     std::vector<int> first{1, 2, 3, 4};
-    std::vector<int> second{1, 2, 3, 4};
+    std::vector<int> second{1, 2, 3};
 
-    std::cout << ArithmeticAverage(first, second) << "\n";
+    try {
+        std::cout << ArithmeticAverage(first, second) << "\n";
+    }
+    catch (std::invalid_argument& ex) {
+        std::cout << ex.what() << '\n';
+    }
 
     return 0;
 }
